65a3a5bb6783806166128222fc1229f3f4bce543
Boluwatife <Boluwatifeb989@gmail.com>
2026-04-25 10:48:07 +0100

    First assignment

---
+++ firstAssignment.js
@@ -0,0 +1,9 @@
+//Declare a variable named 🐰bunny and assign it to an object with the following properties: 
+// name, age, and isHappy. Set the value of name to a string, age to a number, 
+// and isHappy to a boolean.
+
+const bunny ={
+    name: 'Fluffy',
+    age: 2,
+    isHappy: true
+}

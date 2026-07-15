7b4f048d476a4efb97163fa1129385cc0f49d935
Boluwatife <Boluwatifeb989@gmail.com>
2026-07-15 13:42:54 +0100

    Connected PostgreSQL and Configured TypeORM

--- final_assessment/src/server.ts
+++ final_assessment/src/server.ts
@@ -1,10 +1,20 @@
 import dotenv from "dotenv";
 import app from "./app";
+import { AppDataSource } from "./database/data-source";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(` Server is running on http://localhost:${PORT}`);
-});
+AppDataSource.initialize()
+  .then(() => {
+    console.log(" Database connected successfully!");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error(" Database connection failed");
+    console.error(error);
+  });
